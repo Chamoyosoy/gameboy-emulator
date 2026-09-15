a1b38d883570df9a660796d25e664f9477b5e8d0
chamoyosoy <enriquegasca@proton.me>
2026-09-14 21:14:31 -0600

    add structs.h

---
+++ src/structs.h
@@ -0,0 +1,14 @@
+struct registers {
+    unsigned char a;
+    unsigned char b;
+    unsigned char c;
+    unsigned char d;
+    unsigned char e;
+    unsigned char f;
+    unsigned char h;
+    unsigned char l;
+    unsigned char flags;
+    unsigned short sp;
+    unsigned short sp;
+} extern registers;
+
