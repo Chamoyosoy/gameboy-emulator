10f1315ab5a4bd36fb4ea7a9e3ae397f4b5f7412
chamoyosoy <enriquegasca@proton.me>
2026-09-14 19:44:40 -0600

    chage to sdl3 library

--- src/main.c
+++ src/main.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <SDL.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main(void) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("Error inicializando SDL: %s\n", SDL_GetError());
+int main(int argc, char* argv[]) {
+    // 1. Initialize SDL3 Video subsystem
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initialize SDL: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Hola, SDL2 en macOS",
-                                           SDL_WINDOWPOS_CENTERED,
-                                           SDL_WINDOWPOS_CENTERED,
-                                           640, 480, 0);
+    // 2. Create the window
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello SDL3",       // Title
+        640,                // Width
+        480,                // Height
+        SDL_WINDOW_OPENGL   // Flags
+    );
+
     if (!window) {
-        printf("Error creando ventana: %s\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
 
-    // Mantener la ventana abierta
-    bool running = true;
-    while (running) {
-        SDL_Event e;
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                running = false;
+    // 3. Main loop
+    bool done = false;
+    SDL_Event event;
+    while (!done) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                done = true;
             }
         }
+        // Game logic and rendering go here
     }
 
+    // 4. Cleanup
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
-}
-
+}   
